b063abea8bd35cc76c732da83cb840a5a1e3ee6d
Poon <6330137821@student.chula.ac.th>
2022-03-24 16:48:32 +0700

    add VacCenter API

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {getHospitals, getHospital, createHospital, updateHospital, deleteHospital} = require('../controllers/hospitals');
+const {getHospitals, getHospital, createHospital, updateHospital, deleteHospital,getVacCenters} = require('../controllers/hospitals');
 
 //Include other resource routers
 const appointmentRouter = require('./appointments');
@@ -12,6 +12,7 @@ const { getAppointments } = require('../controllers/appointments');
 //Re-route into other resource routers
 router.use('/:hospitalId/appointments/',appointmentRouter);
 
+router.route('/vacCenters').get(getVacCenters);
 router.route('/').get(getHospitals).post(protect,authorize('admin'), createHospital);
 router.route('/:id').get(getHospital).put(protect,authorize('admin'), updateHospital).delete(protect,authorize('admin'),  deleteHospital);
 
