f49ac8abe141dde4bb2b52dfa34ddcaa989f30ee
Poon <6330137821@student.chula.ac.th>
2022-03-31 13:01:54 +0700

    API-security

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,19 @@
 const User = require('../models/User');
 
 //Get tokem from model, create cookie and send response
-const sendTokenResponse=(user, statusCode, res)=>{
+const sendTokenResponse = (user, statusCode, res) => {
     //Create token
-    const token=user.getSignedJwtToken();
-    
+    const token = user.getSignedJwtToken();
+
     const options = {
-        expires:new Date(Date.now()+process.env.JWT_COOKIE_EXPIRE*24*60*60*1000),
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
         httpOnly: true
     };
 
-    if(process.env.NODE_ENV==='production'){
-        options.secure=true;
+    if (process.env.NODE_ENV === 'production') {
+        options.secure = true;
     }
-    res.status(statusCode).cookie('token',token,options).json({
+    res.status(statusCode).cookie('token', token, options).json({
         success: true,
         token
     })
@@ -21,12 +21,12 @@ const sendTokenResponse=(user, statusCode, res)=>{
 //@desc   Register user
 //@route  GET /api/v1/auth/register
 //@access Public
-exports.register= async (req,res,next)=>{
-    try{
-        const {name,email,password,role}=req.body;
+exports.register = async (req, res, next) => {
+    try {
+        const { name, email, password, role } = req.body;
 
         //Create user
-        const user=await User.create({
+        const user = await User.create({
             name,
             email,
             password,
@@ -35,9 +35,9 @@ exports.register= async (req,res,next)=>{
         //Create token
         // const token=user.getSignedJwtToken();
         // res.status(200).json({success:true,token});
-        sendTokenResponse(user,200,res);
-    }catch(err){
-        res.status(400).json({success:false});
+        sendTokenResponse(user, 200, res);
+    } catch (err) {
+        res.status(400).json({ success: false });
         console.log(err.stack);
     }
 };
@@ -45,38 +45,56 @@ exports.register= async (req,res,next)=>{
 //@desc     Login user
 //@route    POST /api/v1/auth/login
 //@access   PUBLIC
-exports.login= async (req,res,next)=>{
-    const {email,password}=req.body;
+exports.login = async (req, res, next) => {
+    try{
+        const { email, password } = req.body;
 
-    //Validate email & password
-    if(!email || !password){
-        return res.status(400).json({success:false,msg:'Please provide an email and password'});
+        //Validate email & password
+        if (!email || !password) {
+            return res.status(400).json({ success: false, msg: 'Please provide an email and password' });
+        }
+        //Check for user
+        const user = await User.findOne({ email }).select('+password');
+        if (!user) {
+            return res.status(400).json({ success: false, msg: 'Invalid credentials' });
+        }
+
+        //Checks if password matches
+        const isMatch = await user.matchPassword(password);
+
+        if (!isMatch) {
+            return res.status(401).json({ success: false, mag: 'Invalid credenials' })
+        }
+
+        //Create token
+        // const token = user.getSignedJwtToken();
+        // res.status(200).json({success:true,token});
+        sendTokenResponse(user, 200, res);
+    }catch(err) {
+        return res.status(401).json({ success: false, msg: 'Cannot convert email or password to string' });
     }
-    //Check for user
-    const user = await User.findOne({email}).select('+password');
-    if(!user){
-        return res.status(400).json({success:false,msg:'Invalid credentials'});
-    }
-
-    //Checks if password matches
-    const isMatch = await user.matchPassword(password);
-
-    if(!isMatch){
-        return res.status(401).json({success:false,mag:'Invalid credenials'})
-    }
-
-    //Create token
-    // const token = user.getSignedJwtToken();
-    // res.status(200).json({success:true,token});
-    sendTokenResponse(user,200,res);
 }
+//@desc     Log user out and Clear cookie
+//@route    GET /api/v1/auth/logout
+//@access   Private
+exports.logout = async (req, res, next) => {
+    res.cookie('token', 'none', {
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly: true
+    });
+
+    res.status(200).json({
+        success: true,
+        data: {}
+    });
+};
 //@desc     Get current Logged in user
 //@route    POST /api/v1/auth/me
 //@access   Private
-exports.getMe= async(req,res,next)=>{
+exports.getMe = async (req, res, next) => {
     const user = await User.findById(req.user.id);
     res.status(200).json({
-        success:true,
-        data:user
+        success: true,
+        data: user
     });
 };
